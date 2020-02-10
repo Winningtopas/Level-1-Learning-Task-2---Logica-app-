e62bb1e3559c9417e06637af7b64ebadab80eab9
Winningtopas <jay07@live.nl>
2020-02-10 13:27:34 +0100

    Started with the code, the code is now able to see if one answer is correct

--- app/src/main/java/com/example/level1task2/MainActivity.kt
+++ app/src/main/java/com/example/level1task2/MainActivity.kt
@@ -2,11 +2,35 @@ package com.example.level1task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
     }
+
+    private fun initViews() {
+        btnConfirm.setOnClickListener { checkAnswer() }
+    }
+
+    private fun onConfirm(){
+
+    }
+
+    private fun checkAnswer() {
+        val answer = answer0.text.toString()
+
+        // When the answer equals "giraffe" then display a correct message using a toast message.
+        // Otherwise display an incorrect message.
+        if (answer == getString(R.string.T)) {
+            Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
